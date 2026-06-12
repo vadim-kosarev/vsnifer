0ed437602cca86b654283e7d812a937a06693ad8
Vadim Kosarev <vadim.kosarev@gmail.com>
2026-06-12 17:14:36 +0300

    updated by claude

--- cron-weekly.cmd
+++ cron-weekly.cmd
@@ -5,5 +5,5 @@ call python .\vk_vsf_bot.py download --all-channels --since 2026-04-20 --count 2
 call python check_ad.py update
 call python check_ad.py update-nudes
 
-start python join_video.py --last-full-days 7 --sort interest-asc --orientation vertical   --audio-delay-ms 0
-start python join_video.py --last-full-days 7 --sort interest-asc --orientation horizontal --audio-delay-ms 0
+call python join_video.py --last-full-days 7 --sort interest-asc --orientation horizontal --audio-delay-ms 0
+call python join_video.py --last-full-days 7 --sort interest-asc --orientation vertical   --audio-delay-ms 0
